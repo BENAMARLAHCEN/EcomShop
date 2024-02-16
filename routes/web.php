86495d1c1e1c575e576f8d6eb3f2af47fa2e23ef
lahcen <lahcen.ben3@gmail.com>
2024-02-16 16:48:39 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
+});
+Route::get('/checkout', function () {
+    return view('products.checkout-page');
 });
 
 
