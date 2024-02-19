b5f25d8745c9e3f2d913a32efaf1cb93f8f8d0de
lahcen <lahcen.ben3@gmail.com>
2024-02-19 09:40:47 +0100

    create product migration and add seed with modell and controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,45 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // $products = [
+        //     [
+        //         'name' => 'Fesh milk 500ML',
+        //         'price' => 250,
+        //         'description' => 'lorem ipsum',
+        //         'image' => 'https://cdn.pixabay.com/photo/2016/12/06/18/27/milk-1887234__340.jpg'
+        //     ],
+        //     [
+        //         'name' => '20 EGGS',
+        //         'price' => 6,
+        //         'description' => 'lorem ipsum',
+        //         'image' => 'https://cdn.pixabay.com/photo/2016/07/23/15/24/egg-1536990__340.jpg'
+        //     ],
+        //     [
+        //         'name' => 'WINE 700ML',
+        //         'price' => 50,
+        //         'description' => 'lorem ipsum',
+        //         'image' => 'https://cdn.pixabay.com/photo/2015/11/07/12/00/alcohol-1031713__340.png'
+        //     ],
+        //     [
+        //         'name' => 'SPEAKER',
+        //         'price' => 12,
+        //         'description' => 'lorem ipsum',
+        //         'image' => 'https://cdn.pixabay.com/photo/2017/01/06/17/49/honey-1958464__340.jpg'
+        //     ]
+        // ];
+        // Product::insert($products);
     }
 }
