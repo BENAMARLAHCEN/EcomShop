c18c88cdac85d752821edb7934f0077f66e3fbd1
lahcen <lahcen.ben3@gmail.com>
2024-02-20 15:28:20 +0100

    create a payment table and a model with controller and method

---
+++ database/migrations/2024_02_20_134156_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_id');
+            $table->string('numero_serie');
+            $table->string('quantity');
+            $table->string('amount');
+            $table->string('currency');
+            $table->string('payment_status');
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+};
