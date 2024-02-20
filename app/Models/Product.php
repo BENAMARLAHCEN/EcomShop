faf7e12abc16ed873627538efc362aca1a7da0fb
lahcen <lahcen.ben3@gmail.com>
2024-02-20 12:33:17 +0100

    add relationship with the category in the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Models\Category;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'image',
+        'description',
+    ];
+
+    public function Category()
+    {
+        return $this->belongsTo(\TCG\Voyager\Models\Category::class);
+    }
+    
 }
