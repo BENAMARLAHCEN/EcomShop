e97ed3c90761ff09fdab85681df13f25c20b6530
lahcen <lahcen.ben3@gmail.com>
2024-02-20 14:17:03 +0100

    create order and order item model with relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 
 class Order extends Model
 {
-    
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_amount',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function or ()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
 }
