81c5c20ef7c653e2d2f7ee032e28a7cbf16f7e62
lahcen <lahcen.ben3@gmail.com>
2024-02-21 13:35:38 +0100

    added  payment table with model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'payment_id',
+        'numero_serie',
+        'quantity',
+        'amount',
+        'currency',
+        'payment_status',
+        'payment_method',
+        'user_id',
+    ];
 }
